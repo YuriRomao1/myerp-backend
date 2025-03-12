e0b1e969864659df5dd3e9a708a8c975f4465e3f
YuriRomao1 <yurijj@hotmail.com>
2025-03-12 19:33:54 -0300

    first commit

---
+++ src/test/java/com/myproject94/myerp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject94.myerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
