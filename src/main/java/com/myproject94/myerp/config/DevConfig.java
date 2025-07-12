a79f95198826d5b8b30576a0a7790aed59a787bd
YuriRomao1 <yurijj@hotmail.com>
2025-07-12 15:08:34 -0300

    modificação do devconfig

--- src/main/java/com/myproject94/myerp/config/DevConfig.java
+++ src/main/java/com/myproject94/myerp/config/DevConfig.java
@@ -3,23 +3,27 @@ package com.myproject94.myerp.config;
 import com.myproject94.myerp.service.DBServices;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("dev")
-@RequiredArgsConstructor
 public class DevConfig {
 
     private final DBServices dbServices;
+    private final String ddlAuto;
 
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private String value;
-
+    @Autowired
+    public DevConfig(DBServices dbServices,
+                     @Value("${spring.jpa.hibernate.ddl-auto}") String ddlAuto) {
+        this.dbServices = dbServices;
+        this.ddlAuto    = ddlAuto;
+    }
     @PostConstruct
     public void instanciaDB() {
-        if ("create".equals(value)) {
+        if (ddlAuto != null && ddlAuto.startsWith("create")) {
             dbServices.instanciaDB();
         }
     }
